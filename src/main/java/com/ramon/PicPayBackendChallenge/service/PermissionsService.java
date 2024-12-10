785fec1509121cbd4039f5b50c1bca606899a783
RamonPontes <rgdpontes@yahoo.com>
2024-12-10 15:51:18 -0300

    Fix PermissionsService

--- src/main/java/com/ramon/PicPayBackendChallenge/service/PermissionsService.java
+++ src/main/java/com/ramon/PicPayBackendChallenge/service/PermissionsService.java
@@ -1,5 +1,8 @@
 package com.ramon.PicPayBackendChallenge.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PermissionsService {
     public boolean hasPermission(int permissions, int permission) {
         return (permissions & permission) == permission;
