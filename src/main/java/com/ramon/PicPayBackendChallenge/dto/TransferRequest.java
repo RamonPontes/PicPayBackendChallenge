8ea243e62cca81117541d5481e6242de7019cc7a
RamonPontes <rgdpontes@yahoo.com>
2024-12-10 12:56:06 -0300

    Add TransferRequest

---
+++ src/main/java/com/ramon/PicPayBackendChallenge/dto/TransferRequest.java
@@ -0,0 +1,33 @@
+package com.ramon.PicPayBackendChallenge.dto;
+
+import java.math.BigDecimal;
+
+public class TransferRequest {
+    private BigDecimal value;
+    private int payer; // send
+    private int payee; // receive
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public int getPayer() {
+        return payer;
+    }
+
+    public void setPayer(int payer) {
+        this.payer = payer;
+    }
+
+    public int getPayee() {
+        return payee;
+    }
+
+    public void setPayee(int payee) {
+        this.payee = payee;
+    }
+}
