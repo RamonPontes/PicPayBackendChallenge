e22a8eabc075dd265de8c98a51d7173552e5ae15
RamonPontes <rgdpontes@yahoo.com>
2024-12-10 12:57:02 -0300

    Add transfer endpoint

--- src/main/java/com/ramon/PicPayBackendChallenge/controller/TransactionController.java
+++ src/main/java/com/ramon/PicPayBackendChallenge/controller/TransactionController.java
@@ -1,4 +1,14 @@
 package com.ramon.PicPayBackendChallenge.controller;
 
+import com.ramon.PicPayBackendChallenge.dto.TransferRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class TransactionController {
+    @PostMapping("/transfer")
+    public ResponseEntity<?> transfer(@RequestBody TransferRequest transferData) {
+        return ResponseEntity.ok(transferData);
+    }
 }
