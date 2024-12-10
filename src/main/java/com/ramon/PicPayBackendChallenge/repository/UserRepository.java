9ccd17e14573121249e27ba7ea3850730e23ef62
RamonPontes <rgdpontes@yahoo.com>
2024-12-10 15:55:03 -0300

    Fix rename

--- src/main/java/com/ramon/PicPayBackendChallenge/repository/UserRepository.java
+++ src/main/java/com/ramon/PicPayBackendChallenge/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.ramon.PicPayBackendChallenge.repository;
 
-import com.ramon.PicPayBackendChallenge.model.PicPayUser;
+import com.ramon.PicPayBackendChallenge.model.Picpayuser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<PicPayUser, Integer> { }
+public interface UserRepository extends JpaRepository<Picpayuser, Integer> { }
