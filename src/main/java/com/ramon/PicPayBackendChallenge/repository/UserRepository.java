a4e999eb2abbe6522b862ff14dea5a6204ba3e0d
RamonPontes <rgdpontes@yahoo.com>
2024-12-11 19:05:32 -0300

    Add findByEmail

--- src/main/java/com/ramon/PicPayBackendChallenge/repository/UserRepository.java
+++ src/main/java/com/ramon/PicPayBackendChallenge/repository/UserRepository.java
@@ -3,4 +3,8 @@ package com.ramon.PicPayBackendChallenge.repository;
 import com.ramon.PicPayBackendChallenge.model.Picpayuser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Picpayuser, Integer> { }
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Picpayuser, Integer> {
+    Optional<Picpayuser> findByEmail(String email);
+}
